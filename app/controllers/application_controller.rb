b498a21a116b511faf479827c292f8dca76e3012
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-09 17:29:08 +0900

    :sparkles: ユーザ画像を追加できるように実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :prefectures, :birthday, :sex])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :prefectures, :birthday, :sex, :image, :image_cache, :introduction, :twitter_url, :instagram_url, :tiktok_url, :facebook_url, :youtube_url, :website_url])
     end
 end
